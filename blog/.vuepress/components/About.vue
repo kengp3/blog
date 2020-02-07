dd30862235526e0fd31be803904ea2ace1474369
Kengp3 <sibawite@gmail.com>
2020-02-07 22:24:59 +0800

    add about me

--- blog/.vuepress/components/About.vue
+++ blog/.vuepress/components/About.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="about-me">
+    <div class="photo-section">
+      <div class="photo" />
+    </div>
     <Content />
   </div>
 </template>
 
 <style lang="stylus">
-  .about-me
-    text-align left
+.about-me
+  text-align center
+
+.photo-section
+  text-align center
+  margin-bottom 10px
+  .photo
+    display inline-block
+    width: 230px
+    height 230px
+    border-radius 50%
+    background-image: url('https://avatars2.githubusercontent.com/u/2957288?s=230&v=4');
 </style>
---
+++ blog/about/README.md
@@ -0,0 +1,10 @@
+---
+permalink: /about
+layout: About
+---
+
+<h1>嗨, 我是 Ken.</h1>
+
+工程師一枚，工作主要是用 `java`, `spring framework`, `struts`, `mybatis`。
+後來跟朋友一起弄專案也漸漸學會了一些前端的東西像是`nodejs`, `react` 略懂一點。
+主要還是是寫**後端**，但好像漸漸轉職成~~全端工程師~~。
